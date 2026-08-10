35f1f98e4f1cf7ee2c6bdf4a46b9fec3590278a9
decelife25-cyber <decelife25@gmail.com>
2026-08-10 14:38:40 +0200

    fix(admin): restore existing product screen after numbering correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainLayout from './components/layout/MainLayout';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Portada from './pages/Portada';
 import Familias from './pages/Familias';
 import ListadoPlatos from './pages/ListadoPlatos';
 import FichaPlato from './pages/FichaPlato';
-import Buscar from './pages/Buscar';
+import Buscador from './pages/Buscador';
 import MiSeleccion from './pages/MiSeleccion';
 import AdminLayout from './components/layout/AdminLayout';
 import Login from './pages/admin/Login';
 import HomePrivado from './pages/admin/HomePrivado';
-import AdminProductos9 from './pages/admin/AdminProductos9';
+import AdminProductos from './pages/admin/AdminProductos';
 import AdminProductoForm from './pages/admin/AdminProductoForm';
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Portada />} />
-          <Route path="familias" element={<Familias />} />
-          <Route path="familias/:id" element={<ListadoPlatos />} />
-          <Route path="plato/:id" element={<FichaPlato />} />
-          <Route path="buscar" element={<Buscar />} />
-          <Route path="seleccion" element={<MiSeleccion />} />
-        </Route>
+        <Route path="/" element={<Portada />} />
+        <Route path="/familias" element={<Familias />} />
+        <Route path="/familia/:id" element={<ListadoPlatos />} />
+        <Route path="/producto/:id" element={<FichaPlato />} />
+        <Route path="/buscar" element={<Buscador />} />
+        <Route path="/seleccion" element={<MiSeleccion />} />
 
         <Route path="/admin/login" element={<Login />} />
-
         <Route path="/admin" element={<AdminLayout />}>
           <Route index element={<HomePrivado />} />
-          <Route path="productos" element={<AdminProductos9 />} />
+          <Route path="productos" element={<AdminProductos />} />
           <Route path="productos/nuevo" element={<AdminProductoForm />} />
           <Route path="productos/:id/editar" element={<AdminProductoForm />} />
         </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
