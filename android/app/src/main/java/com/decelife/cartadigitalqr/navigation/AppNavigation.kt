1d857979ed85606fefe5293f53986c213ee124eb
decelife25-cyber <decelife25@gmail.com>
2026-08-20 18:53:06 +0200

    feat(android): wire product editor navigation

--- android/app/src/main/java/com/decelife/cartadigitalqr/navigation/AppNavigation.kt
+++ android/app/src/main/java/com/decelife/cartadigitalqr/navigation/AppNavigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.rememberNavController
 import com.decelife.cartadigitalqr.screens.ConfiguracionScreen
 import com.decelife.cartadigitalqr.screens.FamiliasScreen
 import com.decelife.cartadigitalqr.screens.HomeScreen
+import com.decelife.cartadigitalqr.screens.ProductoEditorScreen
 import com.decelife.cartadigitalqr.screens.ProductosScreen
 
 @Composable
@@ -21,14 +22,16 @@ fun AppNavigation() {
                 onNavigateToConfiguracion = { navController.navigate("configuracion") }
             )
         }
-        composable("familias") {
-            FamiliasScreen(onBackClick = { navController.popBackStack() })
-        }
+        composable("familias") { FamiliasScreen(onBackClick = { navController.popBackStack() }) }
         composable("productos") {
-            ProductosScreen(onBackClick = { navController.popBackStack() })
+            ProductosScreen(
+                onBackClick = { navController.popBackStack() },
+                onNewProduct = { navController.navigate("producto/nuevo") },
+                onProductClick = { id -> navController.navigate("producto/$id") }
+            )
         }
-        composable("configuracion") {
-            ConfiguracionScreen(onBackClick = { navController.popBackStack() })
-        }
+        composable("producto/nuevo") { ProductoEditorScreen(productId = null, onBack = { navController.popBackStack() }) }
+        composable("producto/{id}") { entry -> ProductoEditorScreen(productId = entry.arguments?.getString("id"), onBack = { navController.popBackStack() }) }
+        composable("configuracion") { ConfiguracionScreen(onBackClick = { navController.popBackStack() }) }
     }
 }
