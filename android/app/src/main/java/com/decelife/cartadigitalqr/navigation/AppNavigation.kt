d49cdb0e95229438cce179ff8f9f986f4491f1fa
decelife25-cyber <decelife25@gmail.com>
2026-08-21 13:21:19 +0200

    Android: conectar navegación del editor de familias

--- android/app/src/main/java/com/decelife/cartadigitalqr/navigation/AppNavigation.kt
+++ android/app/src/main/java/com/decelife/cartadigitalqr/navigation/AppNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.decelife.cartadigitalqr.screens.ConfiguracionScreen
+import com.decelife.cartadigitalqr.screens.FamiliaEditorScreen
 import com.decelife.cartadigitalqr.screens.FamiliasScreen
 import com.decelife.cartadigitalqr.screens.HomeScreen
 import com.decelife.cartadigitalqr.screens.ProductoEditorScreen
@@ -22,7 +23,15 @@ fun AppNavigation() {
                 onNavigateToConfiguracion = { navController.navigate("configuracion") }
             )
         }
-        composable("familias") { FamiliasScreen(onBackClick = { navController.popBackStack() }) }
+        composable("familias") {
+            FamiliasScreen(
+                onBackClick = { navController.popBackStack() },
+                onNewFamily = { navController.navigate("familia/nuevo") },
+                onFamilyClick = { id -> navController.navigate("familia/$id") }
+            )
+        }
+        composable("familia/nuevo") { FamiliaEditorScreen(familiaId = null, onBack = { navController.popBackStack() }) }
+        composable("familia/{id}") { entry -> FamiliaEditorScreen(familiaId = entry.arguments?.getString("id"), onBack = { navController.popBackStack() }) }
         composable("productos") {
             ProductosScreen(
                 onBackClick = { navController.popBackStack() },
