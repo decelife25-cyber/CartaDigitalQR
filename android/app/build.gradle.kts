42fd845c7d1ca770c1a50cf5017ed0ee849dabe1
decelife25-cyber <decelife25@gmail.com>
2026-08-21 15:44:10 +0200

    android: secure Supabase session storage dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.security:security-crypto:1.0.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("io.coil-kt:coil-svg:2.6.0")
 }
